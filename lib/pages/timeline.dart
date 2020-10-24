c23138533dd561f2ae2fc56d4daf1205c8f8adf1
leoBlank <31307382+leoBlank@users.noreply.github.com>
2020-10-24 15:39:12 +0200

    Add files via upload
    https://www.youtube.com/watch?v=g9LdgbxlbgA&list=PL78sHffDjI77iyUND7TrUOXud9NSf0eCr&index=56

---
+++ lib/pages/timeline.dart
@@ -0,0 +1,20 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:updatinguserprofile/widgets/header.dart';
+
+final usersRef = FirebaseFirestore.instance.collection('users');
+
+class Timeline extends StatefulWidget {
+  @override
+  _TimelineState createState() => _TimelineState();
+}
+
+class _TimelineState extends State<Timeline> {
+  @override
+  Widget build(context) {
+    return Scaffold(
+      appBar: header(context, isAppTitle: true),
+      body: Text('Timeline'),
+    );
+  }
+}
